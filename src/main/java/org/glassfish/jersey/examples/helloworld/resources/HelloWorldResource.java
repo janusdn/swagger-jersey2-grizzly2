ef0f96a2afd9143c0e1c1404a9a0ea939f29e886
Janus Dam Nielsen <jdn@fagidiot.dk>
2016-06-27 13:28:51 +0200

    Adding code.

---
+++ src/main/java/org/glassfish/jersey/examples/helloworld/resources/HelloWorldResource.java
@@ -0,0 +1,39 @@
+/*
+Copyright 2016 Janus Friis Nielsen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package org.glassfish.jersey.examples.helloworld.resources;
+
+import io.swagger.annotations.Api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Janus Friis Nielsen (jfn at fagidiot.dk)
+ */
+@Api(value = "Cliched Messages", description = "Access to cliched messages")
+@Path("helloworld")
+public class HelloWorldResource {
+    public static final String CLICHED_MESSAGE = "Hello World!";
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getHello() {
+        return CLICHED_MESSAGE;
+    }
+
+}
